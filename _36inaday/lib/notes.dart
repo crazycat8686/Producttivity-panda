60409885d597cfe42e0b1107efd75c71819dea4d
crazycat8686 <budarajuaravind@gmail.com>
2025-06-25 21:00:05 +0530

    new file for note,making

---
+++ _36inaday/lib/notes.dart
@@ -0,0 +1,5 @@
+import 'dart:ui'; //imagefilter ki avasaram
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:_36inaday/main.dart';
