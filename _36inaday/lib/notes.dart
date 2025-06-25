297642a14b74d636598a3d43f2ec3f2d0c197f73
crazycat8686 <budarajuaravind@gmail.com>
2025-06-25 23:23:00 +0530

    initail page nav 256-3

--- _36inaday/lib/notes.dart
+++ _36inaday/lib/notes.dart
@@ -1,8 +1,7 @@
-import 'dart:ui'; //imagefilter ki avasaram
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:_36inaday/main.dart';
+import 'package:_36inaday/utube.dart';
 
 class note extends StatefulWidget {
   const note({super.key});
@@ -14,12 +13,6 @@ class note extends StatefulWidget {
 class _noteState extends State<note> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsetsGeometry.all(4),
-        child: Column(children: []),
-      ),
-    );
+    return Scaffold(appBar: AppBar());
   }
 }
