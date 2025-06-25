8d35f332b8746c418ca90a4eb497a69303129be6
crazycat8686 <budarajuaravind@gmail.com>
2025-06-25 21:02:27 +0530

    256-2

--- _36inaday/lib/notes.dart
+++ _36inaday/lib/notes.dart
@@ -3,3 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:_36inaday/main.dart';
+
+class note extends StatefulWidget {
+  const note({super.key});
+
+  @override
+  State<note> createState() => _noteState();
+}
+
+class _noteState extends State<note> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(4),
+        child: Column(children: []),
+      ),
+    );
+  }
+}
